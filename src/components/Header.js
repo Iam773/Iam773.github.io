f4318f0699690eb06b80199b7af1c08bc11dda35
Iam773 <flagteamzeeza@gmail.com>
2022-12-15 21:42:55 +0700

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,16 @@ function Header() {
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const [logo_click, set_logo_click] = useState(false);
+    const handle_logo_Click = () => set_logo_click(!logo_click);
+
   return (
     <div className="header">
         <div className="container">
             <div className="header-con">
-                <div className="logo-container">
-                    <a href="#">ReDev <FiCode /></a>
+                <div className={logo_click ? "logo-container scale-out-center" : "logo-container"} onClick={handle_logo_Click}>
+                    <a href="#">{logo_click ? "Main1" : "Main2"} <FiCode /></a>
                 </div>
                 <ul className={click ? "menu active" : "menu"}>
                     <li className='menu-link' onClick={closeMobileMenu}>
