51d1e9a6b6e16833b57a0cc0b09459e34472c1df
773 <flagteamzeeza@gmail.com>
2023-02-12 12:14:04 +0700

    test

--- src/new/home.js
+++ src/new/home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import "./css/theme.css"
 import "./css/Nav.css"
 import "./css/Body.css"
+import "./css/Login.css"
 import { AiOutlineLogin } from "react-icons/ai"
 
 import { FiMenu } from "react-icons/fi"
@@ -10,18 +11,19 @@ import { FaRegMoon , FaMoon } from "react-icons/fa"
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  
+  const [LogMode, setLogMode] = useState(false);
+
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
   return (
     <>
-    <div className="nav-container">
+    <div className={darkMode ? 'nav-container nav-light' : 'nav-container nav-dark'}>
       <span>Home</span>
       <div className="login" onClick={handleClick}>
       <FiMenu />
       </div>
-      <ul className={click ? "menu active" : "menu"}>
+      <ul className={click ? darkMode ? "menu active menu-light" : "menu active menu-dark" : "menu"} onClick={closeMobileMenu}>
                     <li className='menu-link menu-contact' onClick={closeMobileMenu}>
         
                         <h6 onClick={() => setDarkMode(!darkMode)}> 
@@ -38,7 +40,7 @@ function App() {
                         </h6>
                         
                     </li>
-                    <li className='menu-link menu-blog' onClick={closeMobileMenu}>
+                    <li className='menu-link menu-blog' onClick={() => setLogMode(!LogMode)}>
                         {/* <a href="">BLOG</a> */}
                         Login &nbsp; <AiOutlineLogin />
                     </li>
@@ -46,6 +48,22 @@ function App() {
     </div>
 
     <div className={darkMode ? 'body-container light-mode' : 'body-container dark-mode'}>
+                        
+                        <div className={LogMode ? darkMode ? "login-container light" : "login-container dark" : darkMode ? "login-container hide" : "login-container hide"}>
+                          <form>
+                            <h1>LOGIN</h1>
+                            <label>
+                              Username: &nbsp;
+                            <input type="text" name="" id="" />
+                            </label>
+                           
+                            <label>
+                              Passwords: &nbsp;
+                            <input type="password" />
+                            </label>
+                            <input className='' type="submit" />
+                          </form>
+                        </div>
 
     </div>
     </>
